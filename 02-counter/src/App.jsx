122183f01ec41b1815a65ffdb00506ef0dab9aed
Saurav <74979895+k7saurav@users.noreply.github.com>
2024-04-30 01:19:30 +0530

    created counter project with tailwind css

---
+++ 02-counter/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+   return (
+      <>
+         <h1 className="text-2xl font-bold">A Simple Counter Project</h1>
+         <p>Learning the concept of Hooks in React.</p>
+      </>
+   );
+}
+
+export default App;
